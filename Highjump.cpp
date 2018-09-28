1f937f279854943f0f2ba9005d1be5d1c59bb6c7
imsatis <Malsonsatis@outlook.com>
2018-09-28 18:25:35 +0530

    high jump coding

---
+++ Highjump.cpp
@@ -0,0 +1,14 @@
+//High Jump Problem #2
+
+#include<iostream>
+
+using namespace std;
+
+ int main() {
+   
+   int obstacles=0,high_lim=0,distance=0,speed=0;
+   cin>> 
+   int* position = new int[obstacles],*height = new int [obstacles];
+
+
+ }
