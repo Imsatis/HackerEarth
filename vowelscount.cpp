c79340c5a31e4502b21e7838fa6006ae31206828
imsatis <Malsonsatis@outlook.com>
2018-09-29 15:40:18 +0530

    Array #1 problem

---
+++ vowelscount.cpp
@@ -0,0 +1,19 @@
+//counting vowels in the string
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+ int main() {
+    int test_case=0,count;
+    char ch;
+ //   string *character= new string[test_case];
+    cin>>test_case;
+  //  for(int i=0;i<test_case;i++) {
+      while((ch=getch())!=13) {
+          count=0;
+    if(ch==)  //case is to check inserted char is = to AEIOU aeiou Vowels
+   //       character[i]+=ch;
+         }
+      }
+   }
