bfce3eecdcd2a408432ca08c9c1a23ffe5a34977
imsatis <Malsonsatis@outlook.com>
2018-09-30 21:17:39 +0530

    problem solving

--- DataStructures_Array/Binary_Queries.cpp
+++ DataStructures_Array/Binary_Queries.cpp
@@ -4,10 +4,33 @@
 **/
 
 #include<iostream>
+
  
  using namespace std;
 
     int main(void) {
-      
+     int N,Q,i,Num=0;
+     cin>>N,Q;
+     int *Ary = new int[N];
+     for(i=0;i<N;i++) cin>>Ary[i];
+     for (i=0;i<Q;i++) {
+     int temp;cin>>temp;
+     if(temp!=0) {
+       int x;cin>>x;
+       Ary[x-1]==1;
+      }
+     else {
+       int l,r,n=0;
+       cin>>l>>r;
+       for (;l<=r;l++) {
+          if(Ary[l]!=0) Num+=2^n;
+          n++;
+      }
+     } 
+    } 
+    (Num%2!=1) ? cout<<"EVEN" : cout<<"ODD";
+
+     delete Ary;
+     return 0;
     
     }
